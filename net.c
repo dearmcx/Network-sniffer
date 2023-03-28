1a7aece42bd72565a75e804863d996893418bf07
dearmcx <2455692485@qq.com>
2023-03-28 09:45:02 -0700

    edit

--- net.c
+++ net.c
@@ -1,6 +1,7 @@
-#define KBUILD_MODNAME "net"
+
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
+#include <linux/if_arp.h>
 #include <linux/ip.h>
 #include <linux/udp.h>
 #include <linux/tcp.h>
@@ -8,21 +9,49 @@
 #include <linux/icmp.h>
 #include <linux/icmpv6.h>
 #include <linux/in.h>
-
+#include <asm/current.h>
 #include <linux/bpf.h>
 #include <linux/bpf_common.h>
 
+#ifndef memcpy
+#define memcpy(dest, src, n) __builtin_memcpy((dest), (src), (n))
+#endif
 
 BPF_HISTOGRAM(counter, u64);
 BPF_RINGBUF_OUTPUT(buffer, 1 << 4);
 
-struct event {
 
-	u32	saddr;
-	u32	daddr;
+/*
+struct filter
+{
+    //u8 id;
+    //unsigned int enabled : 1;
+    //u8 action;
+    u32 srcip;
+    u32 dstip;
+    //unsigned int do_sport;
+    //unsigned int do_dport;
+};
+*/
+
+
+
+struct event {
+	u16	h_proto;
+	unsigned int	saddr;
+	unsigned int	daddr;
 	u16	sport;
 	u16	dport;
+	u32	seq;
+	u32	ack_seq;
+	u64 ar_sha;
+	u64 ar_tha;
+	u32 ar_sip;
+	u32 ar_tip;
 };
+
+BPF_ARRAY(filter, u32,64);
+
 
 int net_filter(struct xdp_md *ctx)
 {
@@ -42,6 +71,12 @@ int net_filter(struct xdp_md *ctx)
     
     if ((void *)eth + sizeof(*eth) <= data_end)
     {
+    
+    
+    if (eth->h_proto == htons(ETH_P_IP))
+    {
+    
+    
         struct iphdr *iph = data + sizeof(*eth);
         
         
@@ -57,9 +92,26 @@ int net_filter(struct xdp_md *ctx)
         if ((void *)iph + sizeof(*iph) <= data_end)
         {
         
-        //bpf_trace_printk("ip %x ",iph->saddr);
-        event.saddr = iph->saddr;
-        event.daddr = iph->daddr;
+        bpf_trace_printk("sip %d ",iph->saddr);
+        event.h_proto = iph->protocol;
+        event.saddr = cpu_to_be32(iph->saddr);
+        event.daddr = cpu_to_be32(iph->daddr);
+        int key = 0;
+  	unsigned int  *val;
+  	val = filter.lookup(&key);
+  	if (val == NULL) {
+        	return XDP_PASS;
+    	}
+  	bpf_trace_printk("filter : %d ",*val);
+  	bpf_trace_printk("filter : %x ",event.daddr); 
+  	
+  	
+  	
+  	if((*val) == event.saddr){
+  		return XDP_DROP;
+  	}
+        
+        
         switch (iph->protocol)
         {
             case IPPROTO_TCP:
@@ -71,9 +123,12 @@ int net_filter(struct xdp_md *ctx)
                 {
                     return XDP_DROP;
                 }
-                event.sport = tcph->dest;
-                event.dport = tcph->source;                
+                event.sport = cpu_to_be16(tcph->dest);
+                event.dport = cpu_to_be16(tcph->source);                
 		//bpf_trace_printk("tcp %d ",tcph->dest);
+
+		
+		
                 break;
 
             case IPPROTO_UDP:
@@ -85,8 +140,8 @@ int net_filter(struct xdp_md *ctx)
                 {
                     return XDP_DROP;
                 }
-                event.sport = udph->dest;
-                event.dport = udph->source;  
+                event.sport = cpu_to_be16(udph->dest);
+                event.dport = cpu_to_be16(udph->source);  
                 break;
 
             case IPPROTO_ICMP:
@@ -116,6 +171,40 @@ int net_filter(struct xdp_md *ctx)
             }
             */
         }
+    }
+    else if (eth->h_proto == htons(ETH_P_ARP)){
+    	struct arphdr *arph = data + sizeof(*eth);
+    	if ((void *)arph + sizeof(*arph) <= data_end)
+        {
+    		event.h_proto = cpu_to_be16(eth->h_proto);
+    		//memcpy(&event.h_proto,&eth->h_proto,sizeof(event.h_proto));
+    		bpf_trace_printk("arp proto: %d ",event.h_proto);
+    		int offset = sizeof(arph->ar_hrd) +sizeof(arph->ar_pro) +sizeof(arph->ar_op) +sizeof(arph->ar_hln) +sizeof(arph->ar_pln);
+
+    		//bpf_trace_printk("sip %x ",cpu_to_be16(&arph->ar_pro));
+    		/*
+    memcpy(&event.ar_sha, arph+offset, sizeof(event.ar_sha));
+    offset += 6;
+    // 源协议地址
+    memcpy(&event.ar_sip,  arph+offset, sizeof(event.ar_sip));
+    offset += 4;
+    // 目标硬件地址
+    memcpy(&event.ar_tha, arph+offset, sizeof(event.ar_tha));
+    offset += 6;
+    // 目标协议地址
+    memcpy(&event.ar_tip,  arph+offset, sizeof(event.ar_tip));
+    */
+    buffer.ringbuf_output(&event, sizeof(event), 0);
+    
+
+    		//memcpy(&event.ar_sip,&arph->ar_pro,4);
+    		//memcpy(&event.ar_tip,&arph->ar_tip,4);   		
+    	}
+    
+    }
+    
+    
+    
     }
     return XDP_PASS;
 }
--- test.py
+++ test.py
@@ -1,27 +1,42 @@
 from bcc import BPF #1
 from bcc.utils import printb
 import time
+import struct
+import socket
+import ctypes as ct
 
+dist ={6:"tcp",17:"udp",1:"icmp",0x806:"arp"}
 b = BPF(src_file="net.c") #3
 fn = b.load_func("net_filter", BPF.XDP) #4
+
+
 b.attach_xdp("ens33", fn, 0) #5
 
 
+net_fter = b["filter"]
+net_fter[0] =  ct.c_uint(3232287930)
+
 def callback(ctx, data, size):
     event = b['buffer'].event(data)
-    print("saddr:%s sport: %d daddr:%s dport: %d" % (event.saddr, event.sport,event.daddr,event.dport))
-
+    if event.h_proto == 6 or event.h_proto == 17:
+    	print("proto: %s saddr: %s sport: %d daddr: %s dport: %d %d" % (dist[event.h_proto],socket.inet_ntoa(struct.pack('I',socket.htonl(event.saddr))) , event.sport,socket.inet_ntoa(struct.pack('I',socket.htonl(event.daddr))),event.dport,event.daddr))
+    if event.h_proto == 1:
+    	print("proto: %s saddr: %s  daddr: %s " % (dist[event.h_proto],socket.inet_ntoa(struct.pack('I',socket.htonl(event.saddr))) , socket.inet_ntoa(struct.pack('I',socket.htonl(event.daddr)))))
+    if event.h_proto == 2054:
+    	print("proto: %s " % (dist[event.h_proto]))
+	#print(event.ar_sip)
 b['buffer'].open_ring_buffer(callback)
 
 
 try:
     while 1:
-        b.ring_buffer_poll()
+        #b.ring_buffer_poll()
+        b.trace_print()
         # or b.ring_buffer_consume()
         time.sleep(0.5)
 except KeyboardInterrupt:
     sys.exit()
 
-b.remove_xdp(device, 0) #11
+b.remove_xdp("ens33", 0) #11
 
 
